93223f79a8a0a6af08d59805888106b2a2b1c5c3
amjuz <amjupm3@gmail.com>
2024-03-28 03:19:55 +0530

    added sidebar

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ import Link from 'next/link'
 export default function Header() {
 	return (
 		<header className="px-4 py-2.5 flex justify-between items-center gap-32">
-			<section className="flex items-center gap-5">
+			<section className="flex items-center gap-5 pl-3">
 				<button>
 					<Menu />
 				</button>
---
+++ components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { Home, MonitorCheck, MonitorDown, MonitorPlay, SquarePlay } from "lucide-react";
+import { ReactNode } from "react";
+
+
+export default function Sidebar () {
+    return (
+        <div className="flex flex-col w-16 ml-2 mt-3 h-screen gap-8 items-center text-xs">
+            
+                <SideButton text="Home" icon={ <Home/> }/>
+                <SideButton text="Shorts" icon={ <MonitorPlay/> }/>
+                <SideButton text="Subscriptions" icon={ <MonitorCheck/> }/>
+                <SideButton text="You" icon={ <SquarePlay/> }/>
+              
+        </div>
+    );
+}
+
+function SideButton({ text , icon}: { text: string , icon : ReactNode}) {
+    return (
+        <div>
+            <button className="flex flex-col items-center gap-2">
+                {icon}
+                {text}
+            </button>
+        </div>
+    );
+}
